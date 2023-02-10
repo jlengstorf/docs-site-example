1a170ebcdf41c52d3cf55c266dd75e2475ca7ca8
Sean C Davis <scdavis41@gmail.com>
2023-02-10 10:20:31 -0500

    Add body blocks and content from Contentful

--- types/contentful/TypePage.ts
+++ types/contentful/TypePage.ts
@@ -1,11 +1,16 @@
 import * as Contentful from "contentful";
+import { TypeCalloutFields } from "./TypeCallout";
+import { TypeCodeBlockFields } from "./TypeCodeBlock";
 import { TypeHeadingFields } from "./TypeHeading";
+import { TypeImageFields } from "./TypeImage";
+import { TypeListFields } from "./TypeList";
 import { TypeParagraphFields } from "./TypeParagraph";
 
 export interface TypePageFields {
     title: Contentful.EntryFields.Symbol;
+    description: Contentful.EntryFields.Text;
     slug: Contentful.EntryFields.Symbol;
-    sections?: Contentful.Entry<TypeHeadingFields | TypeParagraphFields>[];
+    sections?: Contentful.Entry<TypeCalloutFields | TypeCodeBlockFields | TypeHeadingFields | TypeImageFields | TypeListFields | TypeParagraphFields>[];
 }
 
 export type TypePage = Contentful.Entry<TypePageFields>;
--- types/contentful/index.ts
+++ types/contentful/index.ts
@@ -1,3 +1,8 @@
+export type { TypeBadge, TypeBadgeFields } from "./TypeBadge";
+export type { TypeCallout, TypeCalloutFields } from "./TypeCallout";
+export type { TypeCodeBlock, TypeCodeBlockFields } from "./TypeCodeBlock";
 export type { TypeHeading, TypeHeadingFields } from "./TypeHeading";
+export type { TypeImage, TypeImageFields } from "./TypeImage";
+export type { TypeList, TypeListFields } from "./TypeList";
 export type { TypePage, TypePageFields } from "./TypePage";
 export type { TypeParagraph, TypeParagraphFields } from "./TypeParagraph";
