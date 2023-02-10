3865204c5eb3613ca51f9de5ede98777dd5035c7
Sean C Davis <scdavis41@gmail.com>
2023-02-10 11:30:21 -0500

    Callout styles

--- components/Callout.tsx
+++ components/Callout.tsx
@@ -1,9 +1,13 @@
 import { Callout as CalloutProps } from '@/types';
+import { Icon } from './Icon';
 
 export const Callout: React.FC<CalloutProps> = (props) => {
     return (
-        <p>
-            <code>Callout</code>: {props.body}
-        </p>
+        <div className="flex space-x-3 justify-start items-baseline p-4 bg-slate-100 border border-slate-200 mb-4 last-mb-0 rounded-sm">
+            <span className="w-4 block relative top-[.125rem]">
+                <Icon.Info />
+            </span>
+            <span className="block">{props.body}</span>
+        </div>
     );
 };
