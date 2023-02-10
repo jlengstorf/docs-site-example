12b26ab67ed45dff6b6cbedaff4947b449b50721
Sean C Davis <scdavis41@gmail.com>
2023-02-10 12:29:42 -0500

    Clean up heading styles

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -4,8 +4,8 @@ const headingTagMap: {
     [K in HeadingProps['level']]: { tagName: React.ElementType; className: string };
 } = {
     '1': { tagName: 'h1', className: '' },
-    '2': { tagName: 'h2', className: 'mb-2' },
-    '3': { tagName: 'h3', className: 'mb-1' },
+    '2': { tagName: 'h2', className: 'mb-3 mt-8' },
+    '3': { tagName: 'h3', className: 'mb-2' },
     '4': { tagName: 'h4', className: '' },
     '5': { tagName: 'h5', className: '' },
     '6': { tagName: 'h6', className: '' }
