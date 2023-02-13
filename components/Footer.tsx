703cc3801aaef5df9a70d0b69a7cefa4ceaed381
Sean C Davis <scdavis41@gmail.com>
2023-02-13 15:08:08 -0500

    Adjust footer content

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,12 +4,13 @@ import { Icon } from './Icon';
 export const Footer: React.FC = () => {
     return (
         <div className="py-8 mt-12 border-t border-slate-200 dark:border-slate-600 text-slate-600 dark:text-slate-300">
-            <span className="flex items-center justify-center space-x-2">
-                <span className="block w-5">
+            <span className="flex items-center justify-center space-x-2 text-sm">
+                <span>Built with</span>
+                <span className="block w-4">
                     <Icon.Heart />
                 </span>
-                <span className="text-sm">
-                    Built by{' '}
+                <span>
+                    by{' '}
                     <Link href="https://www.stackbit.com/" className="text-indigo-500 hover:text-indigo-800" target="_blank">
                         Stackbit
                     </Link>
